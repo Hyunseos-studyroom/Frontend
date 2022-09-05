6eaa37defa65bf3028fdcb716631318e8c1d2897
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2022-09-05 12:01:46 +0900

    Update 자료형.js

---
+++ front-end/공부/Java Script/자료형.js
@@ -0,0 +1,9 @@
+//원시타입
+let number = 1; //숫자형(number)
+let str = "abc"; // 문자열형(string)
+let bool = true; // shsflgud(boolen)
+let undi = undefined; // undefined
+let nul = null; // null(object)
+let symbol = Symbol(); // symbol
+
+console.log(typeof(Symbol()))
