1684a090ec5c87f0ec95472f6080b08d992cb7f6
NANONANDFLASH <a24746440@gmail.com>
2022-09-18 22:42:44 +0900

    .

--- front-end/공부/Java Script/함수.js
+++ front-end/공부/Java Script/함수.js
@@ -1,5 +1,11 @@
 function fn() {
+  //함수 선언식
   console.log("Hello World!");
 }
 //함수도 자료형중 하나이기 때문에 변수안에 선언 ㄱㄴ
 fn();
+
+const gugudan = function gugudan() {
+  console.log("3*3=9");
+};
+//함수 표현식은 함수가 할당되어져있는 변수이름으로 나가고 변수이름이 중요함/함수의 이름이 없어도 사용ㄱㄴ==익명함수
--- front-end/공부/Java Script/함수_인수,매개변수.js
+++ front-end/공부/Java Script/함수_인수,매개변수.js
@@ -1,6 +1,7 @@
 function printFruit(name, price) {
+  //매개변수
   console.log(name + "는 " + price + "원 입니다.");
 }
-printFruit("banana", 3000);
+printFruit("banana", 3000); //인수
 printFruit("apple");
 printFruit("orange");
