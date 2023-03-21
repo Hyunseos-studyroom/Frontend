f11b05da015ac8aebc5c4684419a3299588586c6
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2023-03-21 20:21:50 +0900

    0321

---
+++ front-end/공부/js_0321/data types.js
@@ -0,0 +1,7 @@
+const a = true, b = 123.45, c='안녕하세요';
+let x, y = null;
+console.log(a, typeof a);  //boolean == bool
+console.log(b, typeof b);  //number == int, float
+console.log(c, typeof c);  //string == string
+console.log(x, typeof x);  // undigined == 안정해짐 == 비었음
+console.log(y, typeof y); // null==null
---
+++ front-end/공부/js_0321/text data.js
@@ -0,0 +1,34 @@
+let word = '안녕하세요! 🙂';
+console.log(word);
+
+let word1 = '작은따옴표 안에 "큰따옴표" 사용';
+let word2 = "큰따옴표 안에 '작은따옴표' 사용";
+console.log(word1, word2);
+
+let word3 = '안녕하세요~\t\t반갑습니다!\n저는 \\홍길동\\입니다.';
+console.log(word3);  
+
+// let longName = '김수한무 거북이와 두루미 삼천갑자 동방삭 치치카포 사리사리센타 워리워리 세브리깡 무두셀라 구름이 허리케인에 담벼락 담벼락에 서생원 서생원에 고양이 고양이엔 바둑이 바둑이는 돌돌이';
+
+let longName = '김수한무 거북이와 두루미 \
+삼천갑자 동방삭 치치카포 사리사리센타 \
+워리워리 세브리깡 무두셀라 구름이 \
+허리케인에 담벼락 담벼락에 서생원 \
+서생원에 고양이 고양이엔 바둑이 \
+바둑이는 돌돌이';
+
+// ⚠️ 줄바뀜 되는 것이 아님
+// 큰따옴표도 마찬가지
+console.log(longName);  
+
+let word4 = `헬로헬로~ 🤩`;
+console.log(word4);
+
+const NAME = '홍길동';
+let age = 20;
+let married = false;
+
+console.log(
+`제 이름은 ${NAME}, 나이는 ${age}세구요, \
+${married ? '기혼' : '미혼'}입니다.`
+);
