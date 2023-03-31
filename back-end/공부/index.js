6b4bbcb7635987accc98c2eb896be2e3d0308478
정삼복 <68502584+NANONANDFLASH@users.noreply.github.com>
2023-04-01 01:10:27 +0900

    0401

--- back-end/공부/index.js
+++ back-end/공부/index.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
+const port = 3000;
 
-app.get("/", function (req, res) {
-  res.send("hello world");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(3000);
+app.get("/dog", (req, res) => {
+  res.json({ sound: "멍멍" });
+});
+
+app.get("/user/:name", (req, res) => {
+  const { name } = req.params;
+  console.log(name);
+  if (name == "dog") {
+    res.json({ sound: "멍멍" });
+  } else if (name == "cat") {
+    res.json({ sound: "야옹" });
+  }
+});
+
+app.get("/user/:id", (req, res) => {
+  // const q = req.params;
+  // console.log(q.id);
+  const q = req.query;
+  console.log(q);
+  res.json({ user_id: q.id });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
