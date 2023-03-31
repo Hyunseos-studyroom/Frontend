14fa98d1b59d56c8b36865aa7d167034b5d72b88
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2023-03-31 18:07:36 +0900

    3031

--- front-end/공부/js_0320/var.js
+++ front-end/공부/js_0320/var.js
@@ -2,3 +2,9 @@ const SALUTATION = "hello";
 let person = "철수";
 
 console.log(person, SALUTATION);
+
+//변수: let;
+//상수: const 상수명은 대부분 대문자
+// const명과 let명은 중복x
+//한글 등 유니코드 모두 사용가능
+//나머진 다른언어랑 똑같음
--- front-end/공부/js_0321/data types2.js
+++ front-end/공부/js_0321/data types2.js
@@ -1,10 +1,10 @@
-let job = '학생';
+let job = "학생";
 let age = 17;
 
 console.log(job, age);
 
 // 숫자 값이 들어있던 age에 문자열 값을 넣음
-age = '열일곱';
+age = "열일곱";
 
 console.log(age);
 
@@ -13,7 +13,7 @@ console.log(typeof age);
 // 주어진 문자열을 대문자로 바꾸는 함수
 // 다른 자료형에 대한 예외처리 없음
 function getUpperCase(str) {
-    return str.toUpperCase();
-  }
-  
-  console.log(getUpperCase('hello'));
+  return str.toUpperCase();
+}
+
+console.log(getUpperCase("hello"));
