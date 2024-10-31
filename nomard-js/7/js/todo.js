85a1b0ba7d10f9386efe2cb6aa8d639051e2041e
3boku <hyunseojung@clika.io>
2024-10-31 17:50:33 +0900

    7.1

--- nomard-js/7/js/todo.js
+++ nomard-js/7/js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const todoInput = toDoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    todoList.appendChild(li);
+}
+
 function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
-    console.log(newTodo)
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
