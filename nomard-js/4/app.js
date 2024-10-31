fe9836598c0e5194c7695b9310c3563b89e9c602
3boku <hyunseojung@clika.io>
2024-10-31 16:05:37 +0900

    4.6

--- nomard-js/4/app.js
+++ nomard-js/4/app.js
@@ -2,15 +2,27 @@ const loginForm = document.querySelector("#login-form")
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
+const key = "username"
 const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("username", username);
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello, ${username}!`;
+    localStorage.setItem(key, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello, ${username}`;
+}
+const savedUsername = localStorage.getItem(key);
+
+console.log(savedUsername);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
