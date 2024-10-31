9d4924b5334d9ee2b07d776eb1a561a78de641d1
3boku <hyunseojung@clika.io>
2024-10-31 16:35:03 +0900

    5.1

--- nomard-js/5/js/clock.js
+++ nomard-js/5/js/clock.js
@@ -4,4 +4,10 @@ function sayHello() {
     console.log("hello");
 }
 
-setInterval(sayHello, 1000)
+function newData() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+newData()
+setInterval(newData, 1000)
+// setTimeout(sayHello, 5000);
