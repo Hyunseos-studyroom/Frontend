0dabf6ebfc7b74034bfa7e451557cbcd1e20a1f3
3boku <hyunseojung@clika.io>
2024-11-11 15:56:52 +0900

    2

--- TypeScript/2/script.ts
+++ TypeScript/2/script.ts
@@ -1,9 +1,7 @@
-type Age = number
-type Player = {
-    name: string,
-    age?: Age,
-}
 
-const playerMaker = (name: string) : Player => ({name})
-const nico = playerMaker("Nico")
-nico.age = 12
+// 오류남
+
+
+
+
+
